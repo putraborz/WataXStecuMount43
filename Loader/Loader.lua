9d1358e521fcc337246c8041d955c8c753c8adae
WataXScript <dalvo9216@gmail.com>
2025-09-22 22:51:52 +0800

    Update Loader.lua

--- Loader/Loader.lua
+++ Loader/Loader.lua
@@ -1,1 +1,8 @@
+local url = "https://raw.githubusercontent.com/WataXScript/WataXStecuMount43/main/Loader/main.lua"
+local s, e = pcall(function()
+    return loadstring(game:HttpGet(url))()
+end)
 
+if not s then
+    warn("Error load main.lua:", e)
+end
