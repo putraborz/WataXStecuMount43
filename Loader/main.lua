98b4aa7a2904a70d6ab7f5724e2e3657bc18e819
WataXScript <dalvo9216@gmail.com>
2025-10-05 20:56:12 +0800

    Update main.lua

--- Loader/main.lua
+++ Loader/main.lua
@@ -4,7 +4,7 @@ local StarterGui = game:GetService("StarterGui")
 local player = Players.LocalPlayer
 
 local urlVip = "https://raw.githubusercontent.com/WataXScript/VerifikasiScWata/refs/heads/main/Loader/vip.txt"
-local urlSatuan = "https://raw.githubusercontent.com/WataXScript/VerifikasiScWata/refs/heads/main/Loader/stecu.txt"
+local urlSatuan = "https://raw.githubusercontent.com/WataXScript/VerifikasiScWata/refs/heads/main/Loader/10.txt"
 
 local successUrls = {
     "https://raw.githubusercontent.com/WataXScript/WataXMountAtin/main/Loader/WataX.lua",
